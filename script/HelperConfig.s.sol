0eb57776ad061915069cea8cd8d6526af91fa19d
saikrishnacret <saikrishnaask191@gmail.com>
2025-04-02 21:28:44 +0530

    Changed SubId

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -17,7 +17,6 @@ abstract contract CodeConstants {
 }
 
 contract HelperConfig is CodeConstants, Script {
-
     error HelperConfig__InvalidChainId();
 
     struct NetworkConfig {
@@ -32,51 +31,54 @@ contract HelperConfig is CodeConstants, Script {
     }
 
     NetworkConfig public localNetworkConfig;
-    mapping (uint256 chainId => NetworkConfig) networkConfigs;
+    mapping(uint256 chainId => NetworkConfig) networkConfigs;
 
     constructor() {
-        networkConfigs[ETH_SEPOLIA_CHAIN_ID]  = getSepoliaEthConfig();
+        networkConfigs[ETH_SEPOLIA_CHAIN_ID] = getSepoliaEthConfig();
     }
 
-    function getConfigByChainId(uint256 chainId) public returns(NetworkConfig memory){
-        if(networkConfigs[chainId].vrfCoordinator!= address(0)){
+    function getConfigByChainId(
+        uint256 chainId
+    ) public returns (NetworkConfig memory) {
+        if (networkConfigs[chainId].vrfCoordinator != address(0)) {
             return networkConfigs[chainId];
-        } else if(chainId == LOCAL_CHAIN_ID){
+        } else if (chainId == LOCAL_CHAIN_ID) {
             return getOrCreateAnvilEthConfig();
-
-        } else{
+        } else {
             revert HelperConfig__InvalidChainId();
         }
     }
 
-    function getConfig() public returns(NetworkConfig memory){
+    function getConfig() public returns (NetworkConfig memory) {
         return getConfigByChainId(block.chainid);
     }
 
-
-    function getSepoliaEthConfig() public pure returns(NetworkConfig memory){
-        return NetworkConfig ({
-            entranceFee: 0.01 ether,
-            interval: 30,
-            vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
-            gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
-            callbackGasLimit: 500000,
-            subscriptionId:14211736437725299524653069414314714541765775258341705868980757480022497097464,
-            link : 0x779877A7B0D9E8603169DdbD7836e478b4624789,
-            account: 0x46fF1fd941a5EF8df4c57249cabd554D287F2C64
-        });
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: 0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B,
+                gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
+                callbackGasLimit: 500000,
+                subscriptionId: 14211736437725299524653069414314714541765775258341705868980757480022497097464,
+                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+                account: 0x82Ff0ef0428785Ea9Bb8EEE4384925341d6bea44
+            });
     }
 
-    function getOrCreateAnvilEthConfig() public returns(NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
         // check of we see set an active network config
-        if(localNetworkConfig.vrfCoordinator != address(0)){
+        if (localNetworkConfig.vrfCoordinator != address(0)) {
             return localNetworkConfig;
         }
 
         // Deploy mocks and such
         vm.startBroadcast();
         VRFCoordinatorV2_5Mock vrfCoordinatorMock = new VRFCoordinatorV2_5Mock(
-            MOCK_BASE_FEE,MOCK_GAS_PRICE_LINK,MOCK_WEI_PER_UNIT_LINK
+            MOCK_BASE_FEE,
+            MOCK_GAS_PRICE_LINK,
+            MOCK_WEI_PER_UNIT_LINK
         );
         LinkToken linkToken = new LinkToken();
         vm.stopBroadcast();
@@ -87,14 +89,10 @@ contract HelperConfig is CodeConstants, Script {
             vrfCoordinator: address(vrfCoordinatorMock),
             gasLane: 0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae,
             callbackGasLimit: 500000,
-            subscriptionId:0,
+            subscriptionId: 0,
             link: address(linkToken),
             account: 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38
         });
         return localNetworkConfig;
-
     }
-
-
 }
-
